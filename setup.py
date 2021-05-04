d7a48281f1f61b04e05fb115efc66c4b15b4d6fa
Waztom <waztom@gmail.com>
2021-05-04 10:55:29 +0100

    removed rdkit depend req

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         "numpy==1.20.1",
         "matplotlib==3.3.4",
         "svgutils==0.3.4",
-        "rdkit==2020.09.1.0",
     ],
     packages=find_packages(),
     url="https://github.com/Waztom/mscheck",
