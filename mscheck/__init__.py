adabf42b376eb94c13385af5a7daad0de5ae31b1
Waztom <waztom@gmail.com>
2021-05-04 11:09:07 +0100

    Ver update

--- mscheck/__init__.py
+++ mscheck/__init__.py
@@ -1,6 +1,6 @@
 """mscheck initialization."""
 __name__ = "mscheck"
-__version__ = "v0.1.1-beta"
+__version__ = "v0.1.2-beta"
 
 from .spectrum import MassSpectrum
 from .analyse import AnalyseSpectrum
