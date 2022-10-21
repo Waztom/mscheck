30295fa760cb07ed36007ff034c9c40c927112d2
Waztom <waztom@gmail.com>
2022-10-21 20:04:12 +0100

    Less constiantstraintpset rn versidency versions

--- mscheck/__init__.py
+++ mscheck/__init__.py
@@ -1,6 +1,6 @@
 """mscheck initialization."""
 __name__ = "mscheck"
-__version__ = "v0.2.0-beta"
+__version__ = "v0.2.1-beta"
 
 from .spectrum import MassSpectrum
 from .analyse import AnalyseSpectrum
