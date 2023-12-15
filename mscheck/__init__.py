c00eac8beb2fbf30825bf738f9dfe2bb4cf8f733
Waztom <waztom@gmail.com>
2023-12-15 18:26:41 +0000

    Corrected neg mode scan

--- mscheck/__init__.py
+++ mscheck/__init__.py
@@ -1,6 +1,6 @@
 """mscheck initialization."""
 __name__ = "mscheck"
-__version__ = "v0.2.9-beta"
+__version__ = "v0.3.0-beta"
 
 from .spectrum import MassSpectrum
 from .analyse import AnalyseSpectrum
